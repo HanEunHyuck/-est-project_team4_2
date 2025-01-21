0a19793ad083998c9e3c40936af9ea1955f48f7b
cleooo5857 <zleooo@naver.com>
2025-01-21 15:11:43 +0900

    json 파일 생성

--- src/js/index.js
+++ src/js/index.js
@@ -12,32 +12,3 @@ window.addEventListener("DOMContentLoaded", () => {
   dropdown("dropdown1", years);
   dropdown("dropdown2", content);
 });
-
-const getMovies = async  (title, year = '', page = 1) => {
-  const s = `&s=${title}`
-  const y = `&y=${year}`
-  const p = `&page=${page}`
-  try {
-    const res = await fetch(`https://omdbapi.com/?apikey=8e5fae38&s=movie`);
-    console.log(res);
-    
-    const json = await res.json();
-    if (json.Response === 'True') {
-      const { Search: movies, totalResults } = json
-      return {
-        movies,
-        totalResults
-      }
-    }
-    return json.Error
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-const result = async () => {
-  const data = await getMovies('ironman');  
-  console.log(data);
-}
-
-result();
